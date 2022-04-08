fdf11da7a5c90f9bd8df2a0a189a795f273a9a35
Coelacanth19 <87072141+Coelacanth19@users.noreply.github.com>
2022-04-08 10:02:42 +0100

    almost there

--- week5_day4/scatter_plot/Scatter_Plot/app.R
+++ week5_day4/scatter_plot/Scatter_Plot/app.R
@@ -28,7 +28,12 @@ ui <- fluidPage(
           ),
     column(3,
            sliderInput("transparency_input", "Transparency of points", min = 0, max = 1, value = 0.5, step = 0.1, ticks = TRUE)
-          )
+          ),
+    
+    column(3,
+           textInput("title_input", "Title of Graph")
+             
+           ))
     #if you had programmed this portion, you would be home by now
     #if-else statements go here
     #ggtitle is a function of ggplot so should go in output this portion is 
@@ -40,17 +45,26 @@ ui <- fluidPage(
 
 server <- function(input,output, session) {
 
-filtered_data <- reactive(input$color_input,
+inputs_data <- reactive(input$color_input,
                           input$shape_input,
-                          input$
-                          input$text_input)
+                          input$alpha_input,
+                          input$title_input)
+
 }
 
 #what do we want the variables created to do?
 #combine the input from all inputs: color_input, shape_input... to get 
 #plotOutput
 #output$color_input <- render
-
+students_big %>%
+ggplot()
+aes(x = reaction_time, 
+    y= score_in_memory_game, 
+    color = input$color_input,
+    shape = input$shape_input,
+    )
+geom_point(alpha = input$alpha_input) +
+  ggtitle(label = input$text_input)
 #output$shape_input 
 
 
