4ca17a89fdbaf12db47e0da5c43630d8607b83d3
Coelacanth19 <87072141+Coelacanth19@users.noreply.github.com>
2022-04-08 09:46:13 +0100

    possibly added slider scale and ggplot scheme

--- week5_day4/scatter_plot/Scatter_Plot/app.R
+++ week5_day4/scatter_plot/Scatter_Plot/app.R
@@ -9,23 +9,26 @@ ui <- fluidPage(
   # Application title
   titlePanel("A Customisable Scatter Plot"),
   fluidRow(
-    column(4,
+    column(3,
            selectInput("color_input", 
-                       "colour", 
+                       "Colour of Points", 
                        choices = c(Blue = "#3891A6",
                                    Yellow = "#FDE74C", 
                                    Red = "#E3655B") 
              
                       )
            ),
-    column(4,
+    column(3,
            selectInput("shape_input", 
-                       "shapes", 
+                       "Shape of points", 
                        choices = c(Square = 15, 
                                   Circle = 16, 
                                   Triangle = 17)
                        )
-           )
+          ),
+    column(3,
+           sliderInput("transparency_input", "Transparency of points", min = 0, max = 1, value = 0.5, step = 0.1, ticks = TRUE)
+          )
     #if you had programmed this portion, you would be home by now
     #if-else statements go here
     #ggtitle is a function of ggplot so should go in output this portion is 
