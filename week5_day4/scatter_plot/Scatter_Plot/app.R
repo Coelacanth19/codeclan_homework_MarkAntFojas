151762b280bfa9bd99fc4dd31081c2f937de4c03
Coelacanth19 <87072141+Coelacanth19@users.noreply.github.com>
2022-04-09 11:07:08 +0100

    added another fluid row
    still not plotting!

--- week5_day4/scatter_plot/Scatter_Plot/app.R
+++ week5_day4/scatter_plot/Scatter_Plot/app.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
              
                       )
            ),
-    column(3,
+    column(2,
            selectInput("shape_input", 
                        "Shape of points", 
                        choices = c(Square = 15, 
@@ -26,21 +26,23 @@ ui <- fluidPage(
                                   Triangle = 17)
                        )
           ),
-    column(3,
+    column(2,
            sliderInput("transparency_input", "Transparency of points", min = 0, max = 1, value = 0.5, step = 0.1, ticks = TRUE)
           ),
     
-    column(3,
-           textInput("title_input", "Title of Graph"),
-           
-           
-             
-           ),
+
     
-    plotOutput("scatter_plot")
+  fluidRow(
+    column(10,
+           plotOutput("scatter_plot"),
+    column(2,
+                  textInput("title_input", "Title of Graph"),
+           ),
+    plotOutput("scatter_plot") 
+  )
+       
     )
-    #if you had programmed this portion, you would be home by now
-    #if-else statements go here
+  
     #ggtitle is a function of ggplot so should go in output this portion is 
     #UI only
     
@@ -57,7 +59,7 @@ output$scatter_plot <- renderPlot({
   geom_point(color = input$color_input,
             shape = as.numeric(input$shape_input), 
             alpha = as.numeric(input$alpha_input)+
-  ggtitle(label = input$text_input)
+  ggtitle(input$text_input)
   )
 })
 
