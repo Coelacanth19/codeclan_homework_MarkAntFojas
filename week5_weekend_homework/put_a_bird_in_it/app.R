13c805f4c417d53916c53713a0f1ecf083375352
Coelacanth19 <87072141+Coelacanth19@users.noreply.github.com>
2022-04-10 23:13:38 +0100

    working on app
    cleaned up data, presented clean data to app

--- week5_weekend_homework/put_a_bird_in_it/app.R
+++ week5_weekend_homework/put_a_bird_in_it/app.R
@@ -1,13 +1,19 @@
 library(tidyverse)
 library(shiny)
+library(shinythemes)
 library(dplyr)
 library(CodeClanData)
 library(shinythemes)
-
+library(here)
+library(leaflet)
+# Data clean up process is in "week5_weekend_scratch.rmd"
+# loading data
+birds_df <- read_csv(here("clean_data/bird_join.csv"))
 
 ui <- fluidPage(
     # Application title
     titlePanel("Put A Bird On It!"),
+    
     
 )    
 # Run the application 
