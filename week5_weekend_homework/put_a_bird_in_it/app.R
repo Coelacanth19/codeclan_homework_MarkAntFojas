5ac905d590140206e0d82aedaf6106c88a72a0a4
Coelacanth19 <87072141+Coelacanth19@users.noreply.github.com>
2022-04-11 00:46:56 +0100

    making a layer cake
    layers of tabs and sidebars

--- week5_weekend_homework/put_a_bird_in_it/app.R
+++ week5_weekend_homework/put_a_bird_in_it/app.R
@@ -9,20 +9,39 @@ library(leaflet)
 # Data clean up process is in "week5_weekend_scratch.rmd"
 # loading data
 birds_df <- read_csv(here("clean_data/bird_join.csv"))
+simple_bird_name <- c("petrel", "prion", "shearwater", "albatross", "gannet", 
+                      "skua", "penguin", "hoddy", "gull", "albatross", 
+                      "procelllaria", "shag", "tropicbird", "cormorant", 
+                      "booby", "tern", "sheathbill")
 
 ui <- fluidPage(
     shinytheme("superhero"),
     (tabsetPanel(
       tabPanel("Our Data", "contents"),
-      tabPanel("Bird Observation Maps", leafletOutput() ),
-      tabPanel("Observer Map", )
+      tabPanel("Bird Observation Maps", 
+               (sidebarLayout(
+                 sidebarPanel(width = 3, 
+                              selectInput("simple_bird_input", 
+                                          "Select Bird", 
+                                          choices = simple_bird_name)
+               ),
+                  mainPanel(
+                              leafletOutput("bird_map"), width = 10
+                              ),
+      tabPanel("Observer Map", leafletOutput("observer_map"), width = 10 )
     ))
-   
+  
     
     
-)    
+) 
+
+server <- function(input, output, session) {
+  
+}
+  
 # Run the application 
 shinyApp(ui, server)
+
 
 
 
